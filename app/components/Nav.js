c2354e230147566c161f7e1e0ac327cc240886fe
nizamrobin <nizamrobin@gmail.com>
2023-08-04 21:43:21 +0600

    nav and header component done

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,8 +1,15 @@
+"use client";
 import styles from "./Nav.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Nav() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleClick = () => {
+    isMenuOpen ? setIsMenuOpen(false) : setIsMenuOpen(true);
+    // setIsMenuOpen(false);
+  };
   return (
     <nav className={styles.navbar}>
       <Link className={styles.navHead} href="#">
@@ -15,34 +22,43 @@ export default function Nav() {
         />
         <h3 className={styles.navTitle}>nizamrobin</h3>
       </Link>
-      <Image
-        className={`${styles.logo} ${styles.menuLogo} ${styles.menuOpen}`}
-        src="logo/menu.svg"
-        width={500}
-        height={500}
-        alt="Menu"
-      />
-      <Image
-        className={`${styles.logo} ${styles.menuLogo} ${styles.menuClose}`}
-        src="logo/cross.svg"
-        width={500}
-        height={500}
-        alt="Close"
-      />
-      <ul className={styles.navList}>
-        <li className={styles.navListItem}>
-          <a href="#story">Story</a>
-        </li>
-        <li className={styles.navListItem}>
-          <a href="#projects">Projects</a>
-        </li>
-        <li className={styles.navListItem}>
-          <a href="#skill">Skill</a>
-        </li>
-        <li className={styles.navListItem}>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
+      {!isMenuOpen && (
+        <Image
+          className={`${styles.logo} ${styles.menuLogo}`}
+          src="logo/menu.svg"
+          width={500}
+          height={500}
+          alt="Menu"
+          onClick={handleClick}
+        />
+      )}
+      {isMenuOpen && (
+        <>
+          <Image
+            className={`${styles.logo} ${styles.menuLogo}`}
+            src="logo/cross.svg"
+            width={500}
+            height={500}
+            alt="Close"
+            onClick={handleClick}
+          />
+
+          <ul className={styles.navList}>
+            <li className={styles.navListItem} onClick={handleClick}>
+              <a href="#story">Story</a>
+            </li>
+            <li className={styles.navListItem} onClick={handleClick}>
+              <a href="#projects">Projects</a>
+            </li>
+            <li className={styles.navListItem} onClick={handleClick}>
+              <a href="#skill">Skill</a>
+            </li>
+            <li className={styles.navListItem} onClick={handleClick}>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </>
+      )}
     </nav>
   );
 }
