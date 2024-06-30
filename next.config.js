b8197940779b87fa8235a7c5dbbb044244919159
nizamrobin <nizamrobin@gmail.com>
2024-06-30 23:35:49 +0600

    github build

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   // swcMinify: false,
-//   // output: "export",
-//   // images: { unoptimized: true },
-// };
-
-// module.exports = nextConfig;
-
-
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-}
- 
-module.exports = nextConfig
+  // swcMinify: false,
+  output: "export",
+  images: { unoptimized: true },
+};
+
+module.exports = nextConfig;
